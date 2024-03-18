b69f56b23637b90ab669c5f714d9756369d25c66
antonicholakov <antoni.cholakov.123@gmail.com>
2024-03-18 12:01:30 +0200

    changing the IP of the server connection

--- src/test/java/core/BaseTestAndroid.java
+++ src/test/java/core/BaseTestAndroid.java
@@ -37,7 +37,7 @@ public class BaseTestAndroid {
                     .setNoReset(true);
 
             try {
-                driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+                driver = new AndroidDriver(new URL("http://localhost:4723"), options);
                 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
                 // Add a wait for the app to load
