a8943f796b1248ebd64049350f59885e34433645
antonicholakov <antoni.cholakov.123@gmail.com>
2024-03-25 16:37:33 +0200

    issue with WebDriverAgent fix

--- src/test/java/core/BaseTestiOS.java
+++ src/test/java/core/BaseTestiOS.java
@@ -32,7 +32,7 @@ public class BaseTestiOS {
             options.setDeviceName("iPhone 15")
                     .setPlatformVersion("17.0")
                     .setBundleId("com.easysecure")
-                    .setUsePrebuiltWda(true)
+                    .useNewWDA()
                     .setNoReset(true);
 
             // Initialize driver
