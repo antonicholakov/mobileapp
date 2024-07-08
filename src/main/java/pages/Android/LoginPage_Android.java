3250d536c432c507bd967dd697b3605e994a1d31
antonicholakov <antoni.cholakov.123@gmail.com>
2024-07-08 14:29:21 +0300

    fix for the back button locator

--- src/main/java/pages/Android/LoginPage_Android.java
+++ src/main/java/pages/Android/LoginPage_Android.java
@@ -57,7 +57,7 @@ public class LoginPage_Android extends BasePageAndroid {
     @AndroidFindBy(accessibility = "Forgot your password")
     private WebElement forgotYourPasswordMessage;
 
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[1]")
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[1]")
     private WebElement backButtonToLoginScreen;
 
     @AndroidFindBy(accessibility = "Error")
