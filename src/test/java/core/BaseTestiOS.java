854185ca630808b8ead2132b46872a68d2c07bd6
antonicholakov <antoni.cholakov.123@gmail.com>
2025-04-05 11:19:46 +0300

    updated iOS simulator support for 18.3

--- src/test/java/core/BaseTestiOS.java
+++ src/test/java/core/BaseTestiOS.java
@@ -32,8 +32,8 @@ public class BaseTestiOS {
 
             XCUITestOptions options = new XCUITestOptions();
             options.setDeviceName("iPhone 16 Pro Max")
-                    .setPlatformVersion("18.2")
-                    .setUdid("FD32B988-D3F5-40EC-A3E1-7B721A6D98E2")
+                    .setPlatformVersion("18.3")
+                    .setUdid("18416862-6AE6-4941-BE3A-CB40693D52E7")
                     .setCommandTimeouts(Duration.ofSeconds(240))
                     .setWdaLaunchTimeout(Duration.ofSeconds(240))
                     .setBundleId("com.easysecure")
